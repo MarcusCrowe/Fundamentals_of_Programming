85b6cc0536ae23123d859ed9e9fa4fe1d3af851e
Marcus Crowe <marcuscrowe2005@gmail.com>
2024-01-24 13:17:58 -0500

    W2 HW circle area program

---
+++ circle_area.py
@@ -0,0 +1,11 @@
+# Average of two numbers
+number_1 = float(input("Pick a number: "))
+number_2 = float(input("Pick another number: "))
+average = (number_1 + number_2) / 2
+print("The average of both numbers is:", average)
+
+# Area of Circle Calculator
+radius = float(input("Please enter the radius of the circle: "))
+pi = 3.14
+area_of_circle = pi * (radius**2)
+print("The area of the circle is:", area_of_circle)
