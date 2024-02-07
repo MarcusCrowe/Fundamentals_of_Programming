8cc2a94a83704ec2488a67c8c8d84f5382ebf915
Marcus Crowe <marcuscrowe2005@gmail.com>
2024-02-07 14:15:18 -0500

    adding week 4 homework to github

--- Week4_Collections/week4_collections_hw.py
+++ Week4_Collections/week4_collections_hw.py
@@ -1,3 +1,4 @@
+'''
 # Q1
 tuple1 = ("Car", [34, 23, 8], False, [15, 20, 11])
 print(tuple1[3][1])
@@ -22,8 +23,9 @@ set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 set3 = set1.union(set2)
 print(set3)
+'''
 
-#Q6 - NOT FINISHED
+#Q6
 list1 = [3, 6, 9, 12, 15, 18, 21]
 list2 = [4, 8, 12, 16, 20, 24, 28]
 list3 = []
